0d2400286595092e6ab24f317d917dc84cb1aee6
sidz-9 <sid.zaveri.dev@gmail.com>
2023-09-05 20:22:01 +0200

    Added BeerRepository interface.

---
+++ src/main/java/com/example/msscbrewery/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.example.msscbrewery.repositories;
+
+import com.example.msscbrewery.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
